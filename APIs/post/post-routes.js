260288bb3533f0c835cfa60e02d50dca7eb6f7e4
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-11 16:26:44 -0300

    post-routes: ADDED with create route

---
+++ APIs/post/post-routes.js
@@ -0,0 +1,36 @@
+/**
+ * post-routes
+ * /api/post
+ */
+const { Router } = require('express');
+const { check } = require('express-validator');
+
+const router = Router();
+
+/**
+ * @middleware
+ */
+const { fieldsValidation, JWTValidation } = require('../../middleware');
+
+/**
+ * @controllers
+ */
+const { createPost } = require('./post-controllers');
+
+/**
+ * @routes
+ */
+router.post(
+  '/create',
+  [
+    check('content', 'El contenido del post debe tener entre 1 y 155 caracteres.').isLength({
+      min: 1,
+      max: 155,
+    }),
+    JWTValidation,
+    fieldsValidation,
+  ],
+  createPost,
+);
+
+module.exports = router;
