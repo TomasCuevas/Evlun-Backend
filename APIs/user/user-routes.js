bc0d598bd961b1ce3ae3c15d5f6a8b9774f3651c
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-11 06:37:49 -0300

    user-routes: ADDED with signup route

--- APIs/user/user-routes.js
+++ APIs/user/user-routes.js
@@ -1,5 +1,46 @@
+/**
+ * user-routes
+ * /api/user
+ */
 const { Router } = require('express');
+const { check } = require('express-validator');
 
 const router = Router();
+
+/**
+ * @middlewares
+ */
+const fieldsValidation = require('../../middleware/fieldsValidation');
+
+/**
+ * @helpers
+ */
+const { nameValidation, usernameValidation, emailValidation } = require('../../helpers');
+
+/**
+ * @controllers
+ */
+const { userSignup } = require('./user-controllers');
+
+/**
+ * @routes
+ */
+router.post(
+  '/signup',
+  [
+    check('name', 'El nombre debe tener entre 2 y 25 caracteres.').custom(nameValidation),
+    check(
+      'username',
+      'El nombre de usuario debe tener entre 4 y 15 caracteres. (Solo puede tener letras, numeros y guiones)',
+    ).custom(usernameValidation),
+    check('email', 'El email ingresado no es valido.').custom(emailValidation),
+    check('password', 'El password debe tener entre 8 y 30 caracteres.').isLength({
+      min: 8,
+      max: 30,
+    }),
+    fieldsValidation,
+  ],
+  userSignup,
+);
 
 module.exports = router;
