215d194d10ab666ba6444a244aaf0bd7d5519d28
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-12 19:54:19 -0300

    post-controller UPDATED: ADDED the controller to get all posts

--- APIs/post/post-controllers.js
+++ APIs/post/post-controllers.js
@@ -11,7 +11,7 @@ const createPost = async (req = request, res = response) => {
     const { content } = req.body;
 
     // crear post
-    const newPost = await Post({ content, added_by: _id });
+    const newPost = await Post({ content, added_by: _id, date: new Date().getTime() });
     await newPost.save();
 
     // tomar post creado
@@ -37,6 +37,38 @@ const createPost = async (req = request, res = response) => {
   }
 };
 
+const getAllPosts = async (req = request, res = response) => {
+  try {
+    const { limit = 20, skip = 0, lt } = req.query;
+
+    // obtener los posts
+    const posts = await Post.find({ date: { $lt: lt } })
+      .populate('added_by', {
+        username: true,
+        name: true,
+        avatar: true,
+        _id: true,
+      })
+      .skip(skip)
+      .limit(limit)
+      .sort({ date: -1 });
+
+    // respuesta al frontend
+    res.status(200).json({
+      ok: true,
+      msg: 'get all posts',
+      posts,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: 'Contacte con un administrador.',
+    });
+  }
+};
+
 module.exports = {
   createPost,
+  getAllPosts,
 };
