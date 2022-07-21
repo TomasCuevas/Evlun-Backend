4be100299d5db7335ef58f2f88a02b82b56109f7
Tomas Cuevas <memo_mister398@hotmail.com>
2022-07-21 16:00:22 -0300

    post-routes UPDATED: ADDED route to get a post

--- APIs/post/post-routes.js
+++ APIs/post/post-routes.js
@@ -20,11 +20,14 @@ const {
   getAllPosts,
   getPostsByFollowings,
   getUserPosts,
+  getPost,
 } = require('./post-controllers');
 
 /**
  * @routes
  */
+
+// post
 router.post(
   '/create',
   [
@@ -38,14 +41,24 @@ router.post(
   createPost,
 );
 
+// get post
 router.get(
   '/',
+  [check('id', 'El ID del post es necesario.').isMongoId(), fieldsValidation],
+  getPost,
+);
+
+// get profile posts
+router.get(
+  '/user',
   [check('id', 'El ID del usuario es requerido.').isMongoId(), fieldsValidation],
   getUserPosts,
 );
 
+// get all posts
 router.get('/all', [JWTValidation], getAllPosts);
 
+// get followings posts
 router.get('/followings', [JWTValidation], getPostsByFollowings);
 
 module.exports = router;
